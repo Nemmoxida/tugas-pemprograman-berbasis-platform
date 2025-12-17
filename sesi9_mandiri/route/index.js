41c3a42fa24243408e01a097f07b94d9c7f1dfbc
Ilham Fauzy <ilham.kume02@gmail.com>
2025-12-17 13:23:27 +0700

    sesi 11 tugas

--- sesi9_mandiri/route/index.js
+++ sesi9_mandiri/route/index.js
@@ -1,5 +1,16 @@
 import buku from "../useCase/buku.js";
 
+function timeValidation(req, res, next) {
+  const time = new Date();
+  if (time.getHours() >= 8 && time.getHours() <= 18) {
+    const err = new Error("Service unavailable");
+    err.statusCode = 503;
+    return next(err);
+  }
+  console.log(time.getHours());
+  // next();
+}
+
 export default function index(app, express, db) {
-  app.use("/buku", buku(express, db));
+  app.use("/buku", timeValidation, buku(express, db));
 }
